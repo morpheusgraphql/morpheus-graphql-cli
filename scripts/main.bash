289fef9da0a404cca64351301ec19590f5101573
nalchevanidze <d.nalchevanidze@gmail.com>
2019-11-24 01:43:23 +0100

    update

--- scripts/main.bash
+++
@@ -1,5 +0,0 @@
-morpheus build examples/simple.gql examples/api/Simple.hs
-
-morpheus build examples/mutation.gql examples/api/Mutation.hs
-
-morpheus build examples/subscription.gql examples/api/Subscription.hs
